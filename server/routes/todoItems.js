b3035ca433677d707c3150120b57b31e4c8da59b
Johan Sebastian Castro Lopez <sebastiancastro0792@gmail.com>
2023-01-09 00:07:12 -0500

    complete server, fully functional

--- server/routes/todoItems.js
+++ server/routes/todoItems.js
@@ -9,7 +9,7 @@ router.post('/api/item', async (req, res)=>{
     const newItem = new todoItemsModel({
       item: req.body.item
     })
-    //save this item in database
+    //guardar este elemento en la base de datos
     const saveItem = await newItem.save()
     res.status(200).json(saveItem);
   }catch(err){
@@ -17,39 +17,40 @@ router.post('/api/item', async (req, res)=>{
   }
 })
 
-// //create second route -- get data from database
-// router.get('/api/items', async (req, res)=>{
-//   try{
-//     const allTodoItems = await todoItemsModel.find({});
-//     res.status(200).json(allTodoItems)
-//   }catch(err){
-//     res.json(err);
-//   }
-// })
+//  VAMOS a crear una segunda ruta: obtener datos de la base de datos
+
+router.get('/api/items', async (req,res)=>{
+  try{
+    const allTodoItems = await todoItemsModel.find({});
+    res.status(200).json(allTodoItems);
+    }
+    catch(err){
+      req.json(err); 
+    }
+})
+
+//update item
+router.put('/api/item/:id', async (req, res)=>{
+  try{
+    //encuentra el artículo por su id y actualízalo
+    const updateItem = await todoItemsModel.findByIdAndUpdate(req.params.id, {$set: req.body});
+    res.status(200).json(updateItem);
+  }catch(err){
+    res.json(err);
+  }
+})
 
 
-// //update item
-// router.put('/api/item/:id', async (req, res)=>{
-//   try{
-//     //find the item by its id and update it
-//     const updateItem = await todoItemsModel.findByIdAndUpdate(req.params.id, {$set: req.body});
-//     res.status(200).json(updateItem);
-//   }catch(err){
-//     res.json(err);
-//   }
-// })
-
-
-// //Delete item from database
-// router.delete('/api/item/:id', async (req, res)=>{
-//   try{
-//     //find the item by its id and delete it
-//     const deleteItem = await todoItemsModel.findByIdAndDelete(req.params.id);
-//     res.status(200).json('Item Deleted');
-//   }catch(err){
-//     res.json(err);
-//   }
-// })
+//Delete item from database
+router.delete('/api/item/:id', async (req, res)=>{
+  try{
+    //encuentre el elemento por su id y elimínelo
+    const deleteItem = await todoItemsModel.findByIdAndDelete(req.params.id);
+    res.status(200).json('Item Deleted');
+  }catch(err){
+    res.json(err);
+  }
+})
 
 
 //export router
